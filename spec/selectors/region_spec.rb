c2344e3cb600eacf0521c5e00926bcd950f977fe
Sean Doyle <seanpdoyle@users.noreply.github.com>
2022-08-30 15:43:43 -0400

    Introduce `:region` selector (#48)
    While similar to the `:section` selector, the `:region` selector
    _requires_ an `[aria-label]` or `[aria-labelledby]` attribute as a
    locator, instead of an element's first `<h1>` through `<h6>` heading
    element.
    
    In accordance with the [ARIA Authoring Practices: Landmark Regions][]
    section on the [Region][] landmark, add selector support for finding
    `<section>` or `[role="region"]` elements.
    
    A [region][] landmark is a perceivable section of the page containing
    content that is sufficiently important for users to be able to navigate
    to the section.
    
    * A region landmark must have a label.
    * If a page includes more than one region landmark, each should have a
      unique label (see [Step 3][] above).
    * The region landmark can be used identify content that named landmarks
      do not appropriately describe.
    
    [ARIA Authoring Practices: Landmark Regions]: https://www.w3.org/WAI/ARIA/apg/practices/landmark-regions/
    [Region]: https://www.w3.org/WAI/ARIA/apg/practices/landmark-regions/#x4-3-7-region
    [region]: https://w3c.github.io/aria/#region
    [Step 3]: https://www.w3.org/WAI/ARIA/apg/practices/landmark-regions/#aria_lh_step3

---
+++ spec/selectors/region_spec.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "region selector" do
+  describe "locator" do
+    context "when <section> element" do
+      it "finds the first element" do
+        render <<~HTML
+          <section>Content</section>
+        HTML
+
+        expect(page).to have_selector :region, count: 1
+      end
+
+      it "finds based on [aria-label]" do
+        render <<~HTML
+          <section aria-label="Section region">Content</section>
+        HTML
+
+        expect(page).to have_selector :region, "Section region"
+      end
+
+      it "does not find differing on [aria-label]" do
+        render <<~HTML
+          <section aria-label="Section region">Content</section>
+        HTML
+
+        expect(page).to have_no_selector :region, "Not the right locator"
+      end
+
+      it "finds based on [aria-labelledby]" do
+        render <<~HTML
+          <section aria-labelledby="section_label">
+            <h1 id="section_label">Section region</h1>
+          </section>
+        HTML
+
+        expect(page).to have_selector :region, "Section region"
+      end
+
+      it "does not find differing on [aria-labelledby]" do
+        render <<~HTML
+          <section aria-labelledby="section_label">
+            <h1 id="section_label">Section region</h1>
+          </section>
+        HTML
+
+        expect(page).to have_no_selector :region, "Not the right locator"
+      end
+    end
+
+    context "when [role=region]" do
+      it "finds the first element" do
+        render <<~HTML
+          <div role="region">Content</div>
+        HTML
+
+        expect(page).to have_selector :region, count: 1
+      end
+
+      it "finds based on [aria-label]" do
+        render <<~HTML
+          <div role="region" aria-label="Div region">Content</div>
+        HTML
+
+        expect(page).to have_selector :region, "Div region"
+      end
+
+      it "does not find differing on [aria-label]" do
+        render <<~HTML
+          <div role="region" aria-label="Div region">Content</div>
+        HTML
+
+        expect(page).to have_no_selector :region, "Not the right locator"
+      end
+
+      it "finds based on [aria-labelledby]" do
+        render <<~HTML
+          <div role="region" aria-labelledby="region_label">
+            <h1 id="region_label">Div region</h1>
+          </div>
+        HTML
+
+        expect(page).to have_selector :region, "Div region"
+      end
+
+      it "does not find differing on [aria-labelledby]" do
+        render <<~HTML
+          <div role="region" aria-labelledby="region_label">
+            <h1 id="region_label">Div region</h1>
+          </div>
+        HTML
+
+        expect(page).to have_no_selector :region, "Not the right locator"
+      end
+    end
+  end
+end
