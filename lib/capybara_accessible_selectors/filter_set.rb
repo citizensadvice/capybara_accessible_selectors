e9aacb16abdc197bb12192bf4cf8cf14cf17bf87
Daniel Lewis <github@mrdaniellewis.co.uk>
2022-08-30 22:11:50 +0100

    remove focused in favour of upstreamed version

--- lib/capybara_accessible_selectors/filter_set.rb
+++ lib/capybara_accessible_selectors/filter_set.rb
@@ -4,23 +4,19 @@ Capybara::Selector::FilterSet.add(:capybara_accessible_selectors)
 require "capybara_accessible_selectors/filters/current"
 require "capybara_accessible_selectors/filters/described_by"
 require "capybara_accessible_selectors/filters/fieldset"
-require "capybara_accessible_selectors/filters/focused"
 require "capybara_accessible_selectors/filters/validation_error"
 
 {
-  button: %i[focused fieldset],
-  checkbox: %i[focused fieldset described_by validation_error],
-  css: %i[focused],
-  datalist_input: %i[focused fieldset described_by validation_error],
-  element: %i[focused],
-  field: %i[focused fieldset described_by validation_error],
-  file_field: %i[focused fieldset described_by validation_error],
-  fillable_field: %i[focused fieldset described_by validation_error],
-  link: %i[current focused fieldset],
-  link_or_button: %i[current focused fieldset],
-  radio_button: %i[focused fieldset described_by validation_error],
-  select: %i[focused fieldset described_by validation_error],
-  xpath: %i[focused]
+  button: %i[fieldset],
+  checkbox: %i[fieldset described_by validation_error],
+  datalist_input: %i[fieldset described_by validation_error],
+  field: %i[fieldset described_by validation_error],
+  file_field: %i[fieldset described_by validation_error],
+  fillable_field: %i[fieldset described_by validation_error],
+  link: %i[current fieldset],
+  link_or_button: %i[current fieldset],
+  radio_button: %i[fieldset described_by validation_error],
+  select: %i[fieldset described_by validation_error]
 }.each do |selector, filters|
   Capybara.modify_selector(selector) do
     filter_set(:capybara_accessible_selectors, filters)
