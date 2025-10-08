78eb0c2eed954a7e32774d77a0e1c52019a71445
Daniel Lewis <github@mrdaniellewis.co.uk>
2025-10-08 20:39:38 +0100

    allow skipping of drivers for some tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,13 +17,21 @@ Selenium::WebDriver.logger.ignore(:clear_local_storage, :clear_session_storage)
 
 set :public_folder, "./spec/fixtures"
 
+class Object
+  # Provide a way to enter debug when the calling context is a C function
+  # This is generally the case with selectors
+  def _enter_debug(*args, **kwargs)
+    binding.b # rubocop:disable Lint/Debugger
+  end
+end
+
 module CapybaraAccessibleSelectors
   class TestApplication < Sinatra::Application
     get "/pages/new" do
       erb <<~HTML
         <!doctype html>
         <html>
-          <head><meta charset="UTF-8" /></head>
+          <head><meta charset="UTF-8" /><%= params[:head] %></head>
           <body><%= params[:body] %></body>
         </html>
       HTML
@@ -57,13 +65,27 @@ Capybara.register_driver(:edge_headless) do |app|
   options = Selenium::WebDriver::Edge::Options.new(args: ["--headless"])
   Capybara::Selenium::Driver.new(app, browser: :edge, options:)
 end
+Capybara.register_driver(:chrome_canary) do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(
+    binary: "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary"
+  )
+  service = Selenium::WebDriver::Service.chrome(path: "/usr/local/bin/chromedriver-canary/chromedriver")
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:, service:)
+end
+Capybara.register_driver(:chrome_canary_headless) do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(
+    binary: "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary",
+    args: ["--headless"]
+  )
+  service = Selenium::WebDriver::Service.chrome(path: "/usr/local/bin/chromedriver-canary/chromedriver")
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:, service:)
+end
 Capybara.default_driver = driver
 Capybara.app = CapybaraAccessibleSelectors::TestApplication
 Capybara.server = :puma, { Silent: true }
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
-    # Disallow should syntax
     expectations.syntax = :expect
     expectations.max_formatted_output_length = 1000
   end
@@ -72,31 +94,36 @@ RSpec.configure do |config|
     metadata[:type] = :feature
   end
 
+  config.before do |example|
+    next if ENV["IGNORE_DRIVER_SKIPS"]
+
+    skip_driver = Array(example.metadata[:skip_driver])
+    next unless skip_driver.include?(:all) || skip_driver.include?(Capybara.current_driver.to_s.gsub(/_headless$/, "").to_sym)
+
+    skip "not compatible with current driver"
+  end
+
   config.include(Module.new do
     def render(html)
       visit("/pages/new?body=#{CGI.escape(html.strip)}")
     end
+
+    def render_in_head(html)
+      visit("/pages/new?head=#{CGI.escape(html.strip)}")
+    end
   end)
 end
 
-module Capybara
-  module Node
-    module WarnOnTimeouts
-      def synchronize(seconds = nil, *, **)
-        start_time = Time.now
-        super
-      rescue Capybara::ElementNotFound => e
-        seconds ||= Capybara.default_max_wait_time
-        unless seconds.zero? || Time.now - start_time <= seconds
-          stack = caller.select { _1.start_with?(Dir.pwd) }.map { _1.slice(Dir.pwd.length..) }.drop(1)
-          warn("selector has timed out #{stack}")
-        end
-        raise e
-      end
+Capybara::Node::Base.prepend(Module.new do
+  def synchronize(seconds = nil, *, **)
+    start_time = Time.now
+    super
+  rescue Capybara::ElementNotFound => e
+    seconds ||= Capybara.default_max_wait_time
+    unless seconds.zero? || Time.now - start_time <= seconds
+      stack = caller.select { _1.start_with?(Dir.pwd) }.map { _1.slice(Dir.pwd.length..) }.drop(1)
+      warn("selector has timed out #{stack}")
     end
-
-    class Base
-      prepend WarnOnTimeouts
-    end
+    raise e
   end
-end
+end)
